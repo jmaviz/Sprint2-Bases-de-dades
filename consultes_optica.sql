08448c08d659e3c49bfa90bc777af0f5cfc6162f
Juan Martinez Vizcaino <juan.mvx@gmail.com>
2021-09-28 15:38:10 +0200

    Estructura mysql 2

--- consultes_optica.sql
+++
@@ -1,4 +0,0 @@
--- OPTICA
-SELECT * FROM venda WHERE data_venda BETWEEN "2020-01-01" AND "2020-12-30";
-SELECT u.marca FROM ulleres u JOIN venda v ON u.id=v.ulleres_id WHERE data_venda BETWEEN "2020-01-01" AND "2020-12-30";
-SELECT * from proveidor p JOIN ulleres u ON p.DNI=u.DNI_proveidor JOIN venda v ON u.id=v.ulleres_id;
